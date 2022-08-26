1a879c27dc0b3b61f23a89c119d872443e33e5ba
João Luiz <joao.zenos@gmail.com>
2022-08-26 20:15:58 -0300

    BinarySearch: Create Binary Search Implementation with Some Initial Tests

---
+++ src/binary_search.py
@@ -0,0 +1,16 @@
+""" Binary Search Implementation """
+
+def binary_search(list, target):
+    baixo = 0
+    alto = len(list) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        
+        if list[meio] == target:
+            return meio
+        elif list[meio] < target:
+            baixo = meio + 1
+        else:
+            alto = meio - 1
+    return None
---
+++ test/binary_search_test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, main
+from src.binary_search import binary_search
+
+class BinarySearchTest(TestCase):
+
+    def test_should_return_none_when_empty_list(self):
+        self.assertIsNone(binary_search([], 1))
+
+    def test_value_is_not_present(self):
+        self.assertIsNone(binary_search([1,2,3,4,5,6], 7))
+
+    def test_value_is_present(self):
+        self.assertEqual(binary_search(range(1,101), 100), 99)
